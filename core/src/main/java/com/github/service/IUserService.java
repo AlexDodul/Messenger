9ec891175ac0d90827893f82a717ade5a7f1b15c
Aleksandr Dodul <alexdodul87@gmail.com>
2021-04-13 20:22:13 +0300

    add UserService

--- core/src/main/java/com/github/service/IUserService.java
+++ core/src/main/java/com/github/service/IUserService.java
@@ -1,4 +1,21 @@
 package com.github.service;
 
+import com.github.dto.UserAuthDto;
+import com.github.dto.UserRegDto;
+import com.github.entity.User;
+
+import java.util.Collection;
+
 public interface IUserService {
+
+    User findAuth(UserAuthDto userAuthDto);
+
+    User findReg(UserRegDto userRegDto);
+
+    void insert(UserRegDto userRegDto);
+
+    void update(User user);
+
+    void delete(User user);
+
 }
--- core/src/main/java/com/github/service/UserService.java
+++ core/src/main/java/com/github/service/UserService.java
@@ -1,13 +1,48 @@
 package com.github.service;
 
-public class UserService {
-    private UserService(){
+import com.github.dto.UserAuthDto;
+import com.github.dto.UserRegDto;
+import com.github.entity.User;
+import com.github.repository.user.IUserRepository;
 
+public class UserService implements IUserService{
+
+    private final IUserRepository userRepository;
+
+    public UserService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
+    @Override
+    public User findAuth(UserAuthDto userAuthDto) {
+        return this.userRepository.findAuth(userAuthDto);
+    }
+
+    @Override
+    public User findReg(UserRegDto userRegDto) {
+        return this.userRepository.findReg(userRegDto);
+    }
+
+    @Override
+    public void insert(UserRegDto userRegDto) {
+        this.userRepository.insert(userRegDto);
+    }
+
+    @Override
+    public void update(User user) {
+        this.userRepository.update(user);
+    }
+
+    @Override
+    public void delete(User user) {
+        this.userRepository.delete(user);
+    }
+    /*private UserService(){
+
+    }
     private static UserService userService;
 
     public static UserService getUserService() {
         return userService;
-    }
+    }*/
 }

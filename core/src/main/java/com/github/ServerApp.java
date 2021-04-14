98c19e7887a665d89fdfcf99aa8a2b3079c1d63b
Белозерцев Артур <Turabot@i.ua>
2021-04-14 18:45:34 +0300

    tomcat

--- core/src/main/java/com/github/ServerApp.java
+++ core/src/main/java/com/github/ServerApp.java
@@ -1,9 +1,20 @@
 package com.github;
 
+import com.github.config.ServerConfig;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import javax.servlet.ServletException;
+
 public class ServerApp {
 
     public static void main(String[] args) {
-
+        try {
+            Tomcat t = ServerConfig.tomcat();
+            t.start();
+            t.getServer().await();
+        } catch (ServletException | LifecycleException e) {
+            System.out.printf("Enter: %s", e.getMessage());
+        }
     }
 
 }
--- core/src/main/java/com/github/controllers/UsersController.java
+++ core/src/main/java/com/github/controllers/UsersController.java
@@ -1,4 +1,12 @@
 package com.github.controllers;
 
+import com.github.dto.UserAuthDto;
+import com.github.payload.Token;
+
+import java.util.Date;
+
 public class UsersController {
+    public Token auth(UserAuthDto payload) {
+        return new Token(1L, "First Name", "Last Name", new Date(), new Date());
+    }
 }

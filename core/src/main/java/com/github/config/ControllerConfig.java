b8685efa954f1a0036c060d0e2324a19cf46ca81
Aleksandr Dodul <alexdodul87@gmail.com>
2021-04-20 16:49:00 +0300

    dell test.html

--- core/src/main/java/com/github/config/ControllerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.github.config;
-
-import com.github.controllers.UsersController;
-
-public class ControllerConfig {
-    public static UsersController usersController() {
-        return new UsersController(userService);
-    }
-}
--- core/src/main/java/com/github/handlers/WebsocketHandler.java
+++ core/src/main/java/com/github/handlers/WebsocketHandler.java
@@ -1,4 +1,22 @@
 package com.github.handlers;
 
-public class WebsocketHandler{
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+@ServerEndpoint(value = "/chat")
+public class WebsocketHandler {
+
+    @OnOpen
+    public void onOpen(Session session) throws IOException {
+        System.out.println(session.getId());
+    }
+
+    @OnMessage
+    public void onMessage(Session session) throws IOException {
+        System.out.println();
+    }
+
 }
